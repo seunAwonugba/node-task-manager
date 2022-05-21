8a0f3c22ae940910db6b29cc5ba3b3cc7971baa4
seunAwonugba <86803598+seunAwonugba@users.noreply.github.com>
2022-05-21 10:58:14 +0100

    fetch data to ui using axios

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 require("./db/connect");
 const express = require("express");
+//import router
+
+const { router } = require("./routes/projectRouter");
+
 const app = express();
 const port = 3000;
 const host = "localhost";
 
+//serve the static files
+app.use(express.static("./public"));
 //since we will be dealing with json data, because the get request comes in json data, and i will also need to make post request in json data
 //  import express middle ware to handle that 1.
-
 app.use(express.json());
 //without this middle ware, you wont have any data in req.body
 
-//import router 2.
-const { router } = require("./routes/projectRouter");
-
 //use the router 3.
 app.use("/api/v1/tasks", router);
-
-app.get("/", (req, res) => {
-    res.status(200).send("Welcome to the home page");
-});
 
 app.all("*", (req, res) => {
     res.status(400).send("Resource not found");
